e9df6a1f214e470db24523bfb7c79a803afe81ff
bruceblink <likanug.g@qq.com>
2026-06-18 09:43:08 +0800

    feat: update password rule

--- src/views/login/utils/rule.ts
+++ src/views/login/utils/rule.ts
@@ -6,9 +6,9 @@ import { useUserStoreHook } from "@/store/modules/user";
 /** 6位数字验证码正则 */
 export const REGEXP_SIX = /^\d{6}$/;
 
-/** 密码正则（密码格式应为8-18位数字、字母、符号的任意两种组合） */
+/** 密码正则（密码格式应为8-18位数字、字母、符号的任意三种组合） */
 export const REGEXP_PWD =
-  /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)]|[()])+$)(?!^.*[\u4E00-\u9FA5].*$)([^(0-9a-zA-Z)]|[()]|[a-z]|[A-Z]|[0-9]){8,18}$/;
+  /^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[^0-9A-Za-z])[\x21-\x7E]{8,18}$/;
 
 /** 登录校验 */
 const loginRules = reactive<FormRules>({
@@ -19,7 +19,7 @@ const loginRules = reactive<FormRules>({
           callback(new Error("请输入密码"));
         } else if (!REGEXP_PWD.test(value)) {
           callback(
-            new Error("密码格式应为8-18位数字、字母、符号的任意两种组合")
+            new Error("密码格式应为8-18位数字、字母、符号的任意三种组合")
           );
         } else {
           callback();
@@ -113,7 +113,7 @@ const updateRules = reactive<FormRules>({
           callback(new Error("请输入密码"));
         } else if (!REGEXP_PWD.test(value)) {
           callback(
-            new Error("密码格式应为8-18位数字、字母、符号的任意两种组合")
+            new Error("密码格式应为8-18位数字、字母、符号的任意三种组合")
           );
         } else {
           callback();
